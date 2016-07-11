c8807a2e00e2d0ca0992092d427de3518dcb0bd9
John Crocker <jcrocker@zenoss.com>
2016-07-11 15:24:03 -0500

    Download source from zenpip vs apache
    Previously all sources were downloaded from external server; following
    this change they are downloaded from the zenoss pip server.
    
    + A separate makefile is created to enable downloading from original
    sources and uploading to zenoss pip when updating the version.
    
    + Hardcode image versions in makefile (vs. extracting them from a
    versions file.)
    
    + ESAPI.properties is versioned and uploaded to zenpip.

---
+++ versions.mk
@@ -0,0 +1,15 @@
+# Shared definitions for makefiles in download and image subdirectories.
+HBASE_VERSION    := 1.1.4
+OPENTSDB_VERSION := 2.2.0
+HADOOP_VERSION   := 2.5.2
+ZK_VERSION       := 3.4.5
+
+HBASE_IMAGE_VERSION    := v16
+HDFS_IMAGE_VERSION     := v4
+OPENTSDB_IMAGE_VERSION := v23
+
+HBASE_TARBALL    := hbase-$(HBASE_VERSION)-bin.tar.gz
+ESAPI_FILE       := ESAPI-$(HBASE_VERSION).properties
+OPENTSDB_TARBALL := opentsdb-$(OPENTSDB_VERSION).tar.gz
+HADOOP_TARBALL   := hadoop-$(HADOOP_VERSION).tar.gz
+ZK_TARBALL       := zookeeper-$(ZK_VERSION).tar.gz
