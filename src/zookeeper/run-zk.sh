e11ca5256098f6fb272a255cc66a0add533003d6
Oleksandr Styhar <ostyhar@zenoss.com>
2019-09-04 13:44:45 +0000

    CC-4259: stopping services causes nearly all services to be terminate

--- src/zookeeper/run-zk.sh
+++ src/zookeeper/run-zk.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Create the log directory
-mkdir -p /var/log/zookeeper /var/run/zookeeper && chown zookeeper /var/log/zookeeper /var/run/zookeeper
+mkdir -p /var/log/zookeeper /var/run/zookeeper && \
+    chown zookeeper /var/log/zookeeper /var/run/zookeeper && \
+    touch /var/log/zookeeper/zookeeper.log && \
+    chown zookeeper: /var/log/zookeeper/zookeeper.log
 
 # Run ZooKeeper in the foreground
-exec su -s /bin/bash zookeeper -c "JVMFLAGS='-Djava.net.preferIPv4Stack=true' /usr/bin/zookeeper-server start-foreground $@ 2>&1 | tee /var/log/zookeeper/zookeeper.log"
+exec setuser zookeeper /bin/bash -c "JVMFLAGS='-Djava.net.preferIPv4Stack=true' /usr/bin/zookeeper-server start-foreground $@ 2>&1 | tee /var/log/zookeeper/zookeeper.log"
