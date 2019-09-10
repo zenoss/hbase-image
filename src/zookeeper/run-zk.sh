6bd8bbc231a521aa4743f78465af5515075908f9
Oleksandr Styhar <ostyhar@zenoss.com>
2019-09-10 11:33:32 +0000

    fix typo

--- src/zookeeper/run-zk.sh
+++ src/zookeeper/run-zk.sh
@@ -4,7 +4,7 @@
 mkdir -p /var/log/zookeeper /var/run/zookeeper && \
     chown zookeeper /var/log/zookeeper /var/run/zookeeper && \
     touch /var/log/zookeeper/zookeeper.log && \
-    chown zookeeper: /var/log/zookeeper/zookeeper.log
+    chown zookeeper /var/log/zookeeper/zookeeper.log
 
 # Run ZooKeeper in the foreground
 exec setuser zookeeper /bin/bash -c "JVMFLAGS='-Djava.net.preferIPv4Stack=true' /usr/bin/zookeeper-server start-foreground $@ 2>&1 | tee /var/log/zookeeper/zookeeper.log"
